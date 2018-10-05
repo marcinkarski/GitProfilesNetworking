ad0dd670fc2f65c57f083057ff3419dccad609a8
Marcin Karski <karski80@gmail.com>
2018-10-05 18:35:05 +0100

    Initial

--- GitProfilesNetworking/ViewController.swift
+++ GitProfilesNetworking/ViewController.swift
@@ -1,20 +1,48 @@
-//
-//  ViewController.swift
-//  GitProfilesNetworking
-//
-//  Created by Marcin Karski on 05/10/2018.
-//  Copyright © 2018 Marcin Karski. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let users = ["Marcin Karski", "John Sundell", "Todd Kramer"]
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: view.bounds, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(tableView)
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        cell.textLabel?.text = users[indexPath.row]
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        navigationController?.pushViewController(UIViewController(), animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
